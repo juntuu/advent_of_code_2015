717d479873bea39ec3a8e410d04f4c97e50a82bf
Juho Eerola <juho.eerola@tuni.fi>
2019-12-14 22:24:14 +0200

    Day 1, part 2

--- day_01/solution.c
+++ day_01/solution.c
@@ -3,6 +3,7 @@
 
 int main() {
 	int floor = 0;
+	int basement = -1;
 	int done = 0;
 	while (!done) {
 		switch (getchar()) {
@@ -10,7 +11,13 @@ int main() {
 			case ')': floor--; break;
 			case EOF: done = 1; break;
 		}
+		if (basement < 0) {
+			if (floor < 0)
+				basement *= -1;
+			else basement--;
+		}
 	}
 	printf("Day 1, part 1: %d\n", floor);
+	printf("Day 1, part 2: %d\n", basement);
 }
 
