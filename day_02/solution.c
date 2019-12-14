c5b8f2b58d7ab3766aaa9346af97e9ac84b0e524
Juho Eerola <juho.eerola@tuni.fi>
2019-12-14 22:45:57 +0200

    Day 2, part 2

--- day_02/solution.c
+++ day_02/solution.c
@@ -12,11 +12,21 @@ int paper_for_box(int w, int h, int l) {
 	return 2 * (a + b + c) + min(a, min(b, c));
 }
 
+int ribbon_for_box(int w, int h, int l) {
+	int bow = w*h*l;
+	int ribbon = w + h + l + min(-w, min(-h, -l));
+	return 2 * ribbon + bow;
+}
+
 int main() {
 	int paper = 0;
+	int ribbon = 0;
 	int x, y, z;
-	while (scanf("%dx%dx%d", &x, &y, &z) == 3)
+	while (scanf("%dx%dx%d", &x, &y, &z) == 3) {
 		paper += paper_for_box(x, y, z);
+		ribbon += ribbon_for_box(x, y, z);
+	}
 	printf("Day 2, part 1: %d\n", paper);
+	printf("Day 2, part 2: %d\n", ribbon);
 }
 
