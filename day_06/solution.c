70e5c51c4dd0277638440e82c1dfca421d6c49a0
Juho Eerola <juho.eerola@tuni.fi>
2019-12-15 16:37:23 +0200

    Day 6, part 2

--- day_06/solution.c
+++ day_06/solution.c
@@ -6,6 +6,9 @@ enum {
 	TOGGLE = '\0',
 	OFF = 'f',
 	ON = 'n',
+	PLUS2 = '\0' + 1,
+	DECR = 'f' + 1,
+	INCR = 'n' + 1,
 	SIZE = 1000,
 } Grid;
 
@@ -23,6 +26,16 @@ void set_lights(char *grid, int mode, int x0, int y0, int x1, int y1) {
 				case ON:
 					grid[base + i] = 1;
 					break;
+				case PLUS2:
+					grid[base + i] += 2;
+					break;
+				case DECR:
+					if (grid[base + i])
+						grid[base + i]--;
+					break;
+				case INCR:
+					grid[base + i]++;
+					break;
 			}
 		}
 	}
@@ -30,7 +43,8 @@ void set_lights(char *grid, int mode, int x0, int y0, int x1, int y1) {
 
 int main() {
 	char *grid = calloc(SIZE * SIZE, sizeof(char));
-	if (!grid) {
+	char *grid2 = calloc(SIZE * SIZE, sizeof(char));
+	if (!grid || !grid2) {
 		perror("allocating");
 		return 1;
 	}
@@ -38,11 +52,17 @@ int main() {
 	char c[4];
 	while (5 == scanf("%*5c%3s%d,%d %*s %d,%d\n", c, &x0, &y0, &x1, &y1)) {
 		set_lights(grid, c[1], x0, y0, x1, y1);
+		set_lights(grid2, c[1]+1, x0, y0, x1, y1);
 	}
 	int lit = 0;
-	for (int i = 0; i < SIZE * SIZE; i++)
+	int brightness = 0;
+	for (int i = 0; i < SIZE * SIZE; i++) {
 		lit += grid[i];
+		brightness += grid2[i];
+	}
 	printf("Day 6, part 1: %d\n", lit);
+	printf("Day 6, part 2: %d\n", brightness);
 	free(grid);
+	free(grid2);
 }
 
