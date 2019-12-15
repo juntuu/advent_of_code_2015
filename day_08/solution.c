7fd83ebbec3a069e295f0ad5d33ca8347ad6c42e
Juho Eerola <juho.eerola@tuni.fi>
2019-12-15 17:05:42 +0200

    Day 8, part 2

--- day_08/solution.c
+++ day_08/solution.c
@@ -4,20 +4,32 @@
 int main() {
 	int total = 0;
 	int content = 0;
+	int encoded = 0;
 	int c;
 	while ((c = getchar()) != EOF) {
 		if (c == '\n') continue;
 		total++;
-		if (c == '"') continue;
+		if (c == '"') {
+			encoded += 2;
+			continue;
+		}
 		if (c == '\\') {
+			encoded++;
 			total++;
-			if (getchar() == 'x') {
-				getchar();
-				getchar();
-				total += 2;
+			switch (getchar()) {
+				case '"':
+				case '\\':
+					encoded++;
+					break;
+				case 'x':
+					getchar();
+					getchar();
+					total += 2;
+					break;
 			}
 		}
 		content++;
 	}
 	printf("Day 8, part 1: %d\n", total - content);
+	printf("Day 8, part 2: %d\n", encoded);
 }
