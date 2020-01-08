7eb744635020708a6630c2c6f4e569d61c526b27
Juho Eerola <juho.eerola@tuni.fi>
2020-01-09 00:51:25 +0200

    Day 14, part 2

--- day_14/solution.c
+++ day_14/solution.c
@@ -16,6 +16,36 @@ int race(Reindeer r, int time) {
 	return distance + more * r.speed;
 }
 
+int race2(Reindeer *rs, int n, int time) {
+	int best = 0;
+	int *points = calloc(n, sizeof(int));
+	int *distances = calloc(n, sizeof(int));
+	if (!points || !distances) {
+		if (points) free(points);
+		if (distances) free(distances);
+		perror("allocating");
+		return -1;
+	}
+
+	for (int t = 0; t < time; t++) {
+		best = 0;
+		for (int i = 0; i < n; i++) {
+			int flying = t % (rs[i].fly + rs[i].rest) < rs[i].fly;
+			distances[i] += rs[i].speed * flying;
+			if (distances[i] > best) best = distances[i];
+		}
+		for (int i = 0; i < n; i++)
+			points[i] += distances[i] == best;
+	}
+	best = 0;
+	for (int i = 0; i < n; i++)
+		best = points[i] > best ? points[i] : best;
+
+	free(points);
+	free(distances);
+	return best;
+}
+
 int main(int argc, char **argv) {
 	int cap = 9;
 	Reindeer *reindeers = malloc(cap * sizeof(Reindeer));
@@ -46,6 +76,9 @@ int main(int argc, char **argv) {
 		if (distance > best) best = distance;
 	}
 	printf("Day 14, part 1: %d\n", best);
+
+	best = race2(reindeers, n, seconds);
+	printf("Day 14, part 2: %d\n", best);
 
 	free(reindeers);
 }
