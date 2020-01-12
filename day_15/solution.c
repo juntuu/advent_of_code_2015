8f4fa3a885358256abcccfc4ffd42ec58bb16eeb
Juho Eerola <juho.eerola@tuni.fi>
2020-01-12 20:01:41 +0200

    Day 15, part 1

---
+++ day_15/solution.c
@@ -0,0 +1,104 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+	int capacity;
+	int durability;
+	int flavor;
+	int texture;
+	int calories;
+} Ingredient;
+
+typedef struct List {
+	union {
+		void *data;
+		int x;
+	};
+	struct List *next;
+} List;
+
+typedef struct CallBack {
+	void (*call)(struct CallBack*, void *);
+	List *ingredients;
+	int *best;
+} CallBack;
+
+int read_ingredient(FILE *f, Ingredient *i) {
+	return 5 == fscanf(
+			f,
+			"%*s %*s %d, %*s %d, %*s %d, %*s %d, %*s %d",
+			&i->capacity,
+			&i->durability,
+			&i->flavor,
+			&i->texture,
+			&i->calories);
+}
+
+int score(List *ingredients, List *k) {
+	int s = 1;
+	int total[4] = {0, 0, 0, 0};
+	while (k) {
+		Ingredient *i = ingredients->data;
+		total[0] += k->x * i->capacity;
+		total[1] += k->x * i->durability;
+		total[2] += k->x * i->flavor;
+		total[3] += k->x * i->texture;
+		k = k->next;
+		ingredients = ingredients->next;
+	}
+
+	for (int i = 0; i < 4; i++) {
+		if (total[i] < 0) {
+			return 0;
+		}
+		s *= total[i];
+	}
+	return s;
+}
+
+void for_each_combination(int n, int total, List *xs, CallBack *cb) {
+	if (n == 1) {
+		List l = {{.x = total}, xs};
+		cb->call(cb, &l);
+		return;
+	}
+	for (int i = 0; i <= total; i++) {
+		List l = {{.x = i}, xs};
+		for_each_combination(n-1, total-i, &l, cb);
+	}
+}
+
+void improve_score(CallBack *cb, void *k) {
+	int s = score(cb->ingredients, k);
+	if (s > *cb->best)
+		*cb->best = s;
+}
+
+int main() {
+	List ingredients;
+	int n = 0;
+	for (List *i = &ingredients;; i = i->next) {
+		i->next = malloc(sizeof(List));
+		i->data = malloc(sizeof(Ingredient));
+		if (!i->next || !i->x) {
+			fprintf(stderr, "couldn't allocate\n");
+			return 1;
+		}
+		if (!read_ingredient(stdin, i->data))
+			break;
+		n++;
+	}
+
+	int best = 0;
+	CallBack cb = {
+		improve_score,
+		&ingredients,
+		&best,
+	};
+
+	for_each_combination(n, 100, NULL, &cb);
+
+	printf("Day 15, part 1: %d\n", best);
+}
+
