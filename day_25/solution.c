74a03da3b7aad2df04b32138acae12cf5a79fe75
Juho Eerola <juho.eerola@tuni.fi>
2020-09-04 20:13:01 +0300

    Day 25, modular exponentiation instead of iteration

--- day_25/solution.c
+++ day_25/solution.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -27,24 +26,18 @@ int numeric_index(Index i) {
 	return n - 1;
 }
 
-T next_code(T x) {
-	return 252533 * x % 33554393;
-}
-
-T code_rec(Index i) {
-	static T table[6][6] = {
-		{ 20151125, 18749137, 17289845, 30943339, 10071777, 33511524 },
-		{ 31916031, 21629792, 16929656,  7726640, 15514188,  4041754 },
-		{ 16080970,  8057251,  1601130,  7981243, 11661866, 16474243 },
-		{ 24592653, 32451966, 21345942,  9380097, 10600672, 31527494 },
-		{    77061, 17552253, 28094349,  6899651,  9250759, 31663883 },
-		{ 33071741,  6796745, 25397450, 24659492,  1534922, 27995004 },
-	};
-	if (i.col < 7 && i.row < 7) {
-		return table[i.row-1][i.col-1];
+T mod_pow(T base, T exponent, T modulus) {
+	if (modulus == 1)
+		return 0;
+	T result = 1;
+	base %= modulus;
+	while (exponent) {
+		if (exponent & 1)
+			result = (result * base) % modulus;
+		exponent >>= 1;
+		base = (base * base) % modulus;
 	}
-
-	return next_code(code_rec(prev(i)));
+	return result;
 }
 
 T code(Index i) {
@@ -52,13 +45,10 @@ T code(Index i) {
 		fprintf(stderr, "bad index: {%d, %d}\n", i.row, i.col);
 		return 0;
 	}
-	Index base = {6, 6};
-	int n = numeric_index(base);
-	int max = numeric_index(i);
-	if (max <= n) return code_rec(i);
-	T res = code_rec(base);
-	while (n++ < max) res = next_code(res);
-	return res;
+	const T first = 20151125;
+	const T modulus = 33554393;
+	const T multiplier = 252533;
+	return (first * mod_pow(multiplier, numeric_index(i), modulus)) % modulus;
 }
 
 int main(int argc, char **argv) {
